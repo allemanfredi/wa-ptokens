fae186fdf340e17f08cef7d240ccdfc9c8e62b14
allemanfredi <alle.manfredi@gmail.com>
2020-03-25 22:04:55 +0100

    chore: updates readme file

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# wa-ptokens
+# :robot_face: wa-ptokens
 
 Whatsapp bot for interacting with pTokens.
+
+## :page_with_curl: Installation
+
+Register to [twillio](https://www.twilio.com/) and take a sandbox id and a bot telephone number.
+
+Run __`wa-ptokens`__ locally
+
+```
+cargo run
+```
+
+Create a tunnel to localhost (for example with __ngrock__)
+
+```
+ngrock http 8000
+```
+
+Paste the link generated into __twillio__ sandbox.
--- src/deposit_address.rs
+++ src/deposit_address.rs
@@ -2,14 +2,18 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 #[allow(non_snake_case)]
-pub struct Response {
+pub struct DepositAddress {
     enclavePublicKey: String,
     nonce: u32,
     pub nativeDepositAddress: String,
 }
 
+impl DepositAddress {
+  fn verify(&self, )
+}
+
 pub fn get(address: &String, endpoint: &String) -> Result<String, Box<dyn std::error::Error>> {
     let req = format!("{}/get-native-deposit-address/{}", endpoint, address);
-    let resp: Response = reqwest::blocking::get(&req[..])?.json()?;
+    let resp: DepositAddress = reqwest::blocking::get(&req[..])?.json()?;
     Ok(resp.nativeDepositAddress)
 }
